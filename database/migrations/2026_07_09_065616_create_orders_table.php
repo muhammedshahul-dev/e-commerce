c0917303912acc8346bd93ec7fbf1458d0839754
muhammed shahul <muhammedshahullkp@gmail.com>
2026-07-09 21:55:30 +0530

    chore(database): update orders migration schema with correct foreign id

--- database/migrations/2026_07_09_065616_create_orders_table.php
+++ database/migrations/2026_07_09_065616_create_orders_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -13,7 +14,7 @@ return new class extends Migration
     {
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->foreignIdFor(User::class)->constrained()->onDelete('cascade');
             $table->bigInteger('total_amount')->unsigned();
             $table->enum('status', ['pending','processing' ,'shipped','delivered', 'cancelled'])->default('pending');
             $table->text('shipping_address');
